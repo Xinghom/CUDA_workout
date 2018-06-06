489682574aa803967d53f0db2f4f88df1c6108bb
Xinghom <Xinghom@users.noreply.github.com>
2018-06-05 21:03:03 -0700

    sucessfully generated vect

--- multiplication_Matrix-Vector/mul_mat-vet.cu
+++ multiplication_Matrix-Vector/mul_mat-vet.cu
@@ -18,23 +18,33 @@ __global__ void mat_vect(int *a, int *b, int *c, int m, int n) {
 }
 
 void TestMatrixGenerate(int* matrix, int row, int col) {
-    printf("Row: %d", row);
-    printf("Col: %d", col);
+    //printf("Row: %d", row);
+    //printf("Col: %d", col);
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
             matrix[i * col + j] = (j + 1) + i * col; 
-            printf("%d ", matrix[i*col + j]);
+      	    //printf("%d ", matrix[i*col + j]);
         }
-        printf("\n");
+        //printf("\n");
+    }
+}
+
+void TestVectorGenerate(int* vector, int row) {
+    for(int i = 0; i < row; i++) {
+	vector[i] = i * 2;
+ 	//printf("%d ", vector[i]);
     }
 }
 
 int main() {
     int m[ROW*COL];
-    int *ptr_m;
+    int v[COL];
+
+    int *ptr_m, *ptr_v;
     ptr_m = m;
+    ptr_v = v;
     TestMatrixGenerate(ptr_m, ROW, COL);
-    
+    TestVectorGenerate(ptr_v, ROW);
     
 
     return 0;
