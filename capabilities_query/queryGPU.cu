25926ae7ee0260a54e2f31e5872e34765a260fb8
Xinghom <Xinghom@users.noreply.github.com>
2018-06-05 18:17:08 -0700

    GPU prop query

--- capabilities_query/queryGPU.cu
+++ capabilities_query/queryGPU.cu
@@ -1,5 +1,26 @@
+/*
+To query the number of CUDA-capable GPUs
+in a host and the capabilities of each GPU. 
+Run it on the Hummingbird GPU node and 
+report the results.
+*/
+
 #include<stdio.h>
 
 void main() {
-
+    int nDevives;
+    cudaGetDeviceCount(&nDevices);
+    for (int i = 0; i < nDevices; i++) {
+        cudaDeviceProp prop;
+        cudaGetDeviceProperties(&prop, i);
+        printf("GPU Num: %d\n", i);
+        printf("  - GPU Name: %s\n", prop.name);
+        printf("  - Mem Speed: %d\n", prop.memoryClockRate);
+        printf("  - Mem Bus Width (bits): %d\n", prop.memoryBusWidth);
+        float MemPeakBandWidth = 2.0 * prop.memoryClockRate * (prop.memoryBusWidth/8) / 1.0e6;
+        printf("  - Mem Peak Bandwidth(GB/s): %f\n", MemPeakBandWidth);
+        printf("*** Compute Capability: ");
+        printf(prop.major.minor);
+        printf("\n");
+    }
 }
